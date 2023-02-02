a77226715bf09afe90848aa317bda5874ed40c36
Martin Smith <martin.smith@clear.bank>
2023-02-02 22:33:27 +0000

    Project .NET versions

--- src/SQLCover/test/DatabaseWithTests/tSQLt/Views/Tests.sql
+++ src/SQLCover/test/DatabaseWithTests/tSQLt/Views/Tests.sql
@@ -1,8 +1,8 @@
 ﻿
 CREATE VIEW tSQLt.Tests
 AS
-  SELECT classes.SchemaId, classes.Name AS TestClassName, 
-         procs.object_id AS ObjectId, procs.name AS Name
+  SELECT classes.SchemaId, classes.Name AS TestClassName,
+         master.procs.object_id AS ObjectId, procs.name AS Name
     FROM tSQLt.TestClasses classes
     JOIN sys.procedures procs ON classes.SchemaId = procs.schema_id
-   WHERE LOWER(procs.name) LIKE 'test%';
+   WHERE LOWER(master.procs.name) LIKE 'test%';
